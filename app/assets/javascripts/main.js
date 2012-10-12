c787492bd76c5ff13a37c38ffd73ebc63a51a7ac
Phil Matranga <phil@dmgctrl.com>
2012-10-12 15:08:54 -0400

    updated css tooltips

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,18 +1,17 @@
-function displayDirection(stations, name){
+function displayDepartureTable(stations, name){
  $('li a').each(function(i){
 		$(this).append("<div class='"+name+"' id='"+name+i+"'><p>table <br>TABLE</p></div>");
 		var my_tooltip = $("#"+name+i);
 
 		$(this).hover(function(){
-			event.preventDefault()
 				my_tooltip.css({opacity:0.8, display:"none"}).fadeIn(300);
 		}).mousemove(function(kmouse){
 				my_tooltip.css({left:kmouse.pageX+10, top:kmouse.pageY+10});
 		}).mouseout(function(){
 				my_tooltip.stop(true, true).fadeOut(100);
 		});
-	});
+	});	
 }
 $(document).ready(function(){
-	 displayDirection("a","tooltip");
+	 displayDepartureTable("a","tooltip");
 });
